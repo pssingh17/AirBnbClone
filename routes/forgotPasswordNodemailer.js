8e8598c1dd29c7157993e57e94e7f4b078c251b9
PS <pssingh1789@gmail.com>
2023-02-16 22:09:25 -0500

    1313

--- routes/forgotPasswordNodemailer.js
+++ routes/forgotPasswordNodemailer.js
@@ -39,8 +39,6 @@ router.post("/", async (req, res) => {
         const response = await User.findOneAndUpdate({email:data.email},{verifyToken:randomVerifyCode},{password:0,verifyToken:0})
         var transporter = nodemailer.createTransport({
           service: "gmail",
-          port: 587,
-          secure: true,
           auth: {
             user: "t39200309@gmail.com",
             pass: process.env.NodeMailerPass,
@@ -72,36 +70,33 @@ router.post("/", async (req, res) => {
      }
         
       
-    } else if (data.userType==="Host"){
+    } else {
       
         const emailExist = await Model.findOne({ email: data.email });
-        console.log("emailExist", emailExist)
+        // console.log("emailExist", emailExist)
         if (emailExist) {
           const response = await Model.findOneAndUpdate({email:data.email},{verifyToken:randomVerifyCode},{password:0,verifyToken:0})
           var transporter = nodemailer.createTransport({
             service: "gmail",
-            port: 587,
-            secure: true,
             auth: {
               user: "t39200309@gmail.com",
               pass: process.env.NodeMailerPass,
             },
           });
-  
+
           var mailOptions = {
             from: "t39200309@gmail.com",
             to: data.email,
             subject: "Verify Your Email",
             text: `Verification code is - ${randomVerifyCode}`,
           };
-  
+
           transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
               console.log(error);
             } else {
                
               console.log("Email sent: " + info.response);
-              
               res.json({message:"Verify Your email", response:response.verified})
             }
           });
